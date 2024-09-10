5a134034a19746f7ec786ab7aafbfa919fcf1d81
Ness Reliford <nessreliford@gmail.com>
2024-09-10 15:02:05 -0700

    WIP

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
   state = {
     todos: [],
     error: '',
@@ -41,6 +42,17 @@ export default class App extends React.Component {
     .catch(this.setAxiosResponseError)
   }
 
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+    .then(res => {
+      this.setState({...this.state, todos: this.state.todos.map(td => {
+        if(td.id !== id) return td 
+        return res.data.data
+      })})
+    })
+    .catch(this.setAxiosResponseError)
+  }
+
   componentDidMount(){
     this.fetchAllTodos()
   }
@@ -53,7 +65,7 @@ export default class App extends React.Component {
         <h2>Todos:</h2>
         {
           this.state.todos.map(td => {
-            return <div key={td.id}>{td.name}</div>
+            return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name}{td.completed ? " ✔️" :''}</div>
           })
         }
         <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
